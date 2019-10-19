1f2636bb78c8db6bc3abf963bebe059a7c5c3964
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2019-10-19 02:11:46 -0400

    Fixed go module information

--- swagger.sh
+++ swagger.sh
@@ -11,4 +11,5 @@
 export GO_POST_PROCESS_FILE="/usr/local/bin/gofmt -w"
 
 # Generate SiaStats client with Swagger
-openapi-generator generate -i https://raw.githubusercontent.com/jkawamoto/siastats-swagger/master/swagger.yaml -g go -o ./pkg --package-name siastats
+openapi-generator generate -i https://raw.githubusercontent.com/jkawamoto/siastats-swagger/master/swagger.yaml \
+    -g go -o ./pkg --package-name siastats --git-user-id jkawamoto --git-repo-id go-siastats
